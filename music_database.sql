aa072729c55d845844816289375d2c2560ea6af0
umashankarray8409 <umashankarray8409@gmail.com>
2025-01-17 12:50:02 +0530

    second commit

--- music_database.sql
+++ music_database.sql
@@ -1,2 +1,6 @@
-Q1. Who is the senior most employee based on job titile?
-Q2. which countries have the most invoices?
+write a query from invoice and invoice_line where invoice_id start with ascending
+
+select invoice.invoice_id 
+from invoice join invoice_line on
+invoice.invoice_id = invoice_line.invoice_id
+order by invoice_id desc 
