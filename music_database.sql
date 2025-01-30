822b19557f0175f8eb8b9511ec21870c17f1db57
umashankarray8409 <umashankarray8409@gmail.com>
2025-01-30 19:34:29 +0530

    last 3 group by

--- music_database.sql
+++ music_database.sql
@@ -1,61 +1,73 @@
-select * from track
+Q.List all the customers in the music store
+Select * from customer
 
+Q.Find all albums available in the store.
+select * from album
 
-Q1 show from travel grouping with greatest and least?
-Select greatest(source,destination), least(source, destination), distance
-from track
-group by greatest(source, destination), least(source, destination)
+Q.Show all songs (tracks) and their durations.
+select name,milliseconds from track 
 
-who is the senior most employee based on job titile?
+Q.Get a list of all artists in the store.
+select name,artist_id from artist
 
-Select * from employee 
-order by levels desc
-limit 1;
+Q.Find all tracks that are longer than 5 minutes.
+select * from track where milliseconds>3000
 
-which country has the most Invoices?
+Q.List the top 10 most expensive tracks
+select * from track order by unit_price desc
+limit 10;
 
-select count(*), billing_country
-from invoice
-group by billing_country
-order by count(*) 
+Q. Get a list of all tracks with their album names.
+select track.name, track.album_id from track inner join album on track.album_id=album.album_id
+
+Q.Find all albums by a specific artist (e.g., 'The Beatles').
+select art.name from album as alb inner join artist as art on alb.artist_id = art.artist_id 
+where art.name = 'The Beatles'
+
+Q.Show all customers and their company history
+SELECT concat(cst.first_name,cst.last_name), inv.total, inv.invoice_id FROM customer as cst 
+JOIN invoice as inv ON cst.customer_id = inv.customer_id;
+
+Q.Find the total number of tracks in the store.
+Select count(*) as total from track 
+
+Q.Find the most popular genre based on the number of tracks.
+SELECT gr.name, COUNT(tr.track_id) AS track_count FROM genre
+JOIN track ON gr.genre_id = tr.track_id
+GROUP BY gr.name ORDER BY track_count DESC
+LIMIT 1;
+
+Q.Find the total sales revenue from all invoices.
+select sum(total) as total_sale from invoice
+
+Q.Find the artist with the most albums.
+select art.name, count(al.album_id) as most_album 
+from artist as art join album as al 
+on art.artist_id = al.artist_id
+group by art.name
+order by most_album desc
 limit 1
 
-what are top 3 values of total invoices?
-
-select * 
-from invoice 
+Q: Find the top 5 highest spending customers.
+select concat(cus.first_name,cus.last_name),total
+from customer as cus join invoice as inv
+on cus.customer_id =inv.customer_id
 order by total desc
-limit 3
+limit 5
 
-which country has the best customers.
-We would like to	throw a promotional music festival in the city we made the most money. 
-Write a query  that returns that one has the highest sum of invoice totals. 
-return both the city name & sum of all invoice totals
 
-Select sum(total) as invoice_total, billing_city
-from invoice
-group by billing_city
-order by invoice_total desc
 
-write a query to return the first_name, last_name, email & genre of all rock music listeners.
-Return your list ordered alphabetically by email starting with A
 
-SELECT DISTINCT email, first_name, last_name
-FROM customer
-JOIN invoice ON customer.customer_id = invoice.customer_id
-JOIN invoice_line ON invoice.invoice_id = invoice_line.invoice_id
-WHERE track_id IN(
-	SELECT track_id FROM track
-	JOIN genre ON track.genre_id = genre.genre_id
-	WHERE genre.name LIKE 'Rock'
-)
-ORDER BY email;
 
-write a query to return the email, first name, last name & Genre of all rock music listener. 
-Return your list ordered alphabetically by email staring with a.
-select first_name, last_name, genre 
-from customer
-where email like 'a%' group by email order by email desc
+
+
+
+
+
+
+
+
+
 
 
 
